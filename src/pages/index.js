6b3ac7c662f2027648c19e7ad54f4b110ea82bae
Thomasin Durgin <td@thomasin.com>
2020-12-10 16:57:17 -0600

    added headings to galleries

--- src/pages/index.js
+++ src/pages/index.js
@@ -28,7 +28,17 @@ const IndexPage = () => (
     <div className="col-6 col-md-4 col-lg-3">
     <a href="/page-8/"><img src={ringImagesr1a} alt="Ring sr1a" class="thumbnail"/></a>
     </div>
-   
+  </div>
+  <div className="row">
+    <div className="col-6 col-md-4 col-lg-3">
+    <h6>Every Day Rings</h6>
+    </div>
+    <div className="col-6 col-md-4 col-lg-3">
+    <h6>Unusual Rings</h6>
+    </div>
+    <div className="col-6 col-md-4 col-lg-3">
+    <h6>Rings as Sculpture</h6>
+    </div>
   </div>
 </div>
     
